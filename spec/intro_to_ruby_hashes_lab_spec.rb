85021b4a6a5875beeb0951964d9cea96ef1842c1
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-22 18:31:49 +0000

    automatically backed up by learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -131,7 +131,7 @@ describe "building a multidimensional monopoly hash" do
       it "sets the 1st key of the :pennsylvania_railroad hash to a string, 'mortgage_value', whose value is '$200'" do
 
         expect(monopoly_with_fourth_tier.values[0][:names].has_key?(:pennsylvania_railroad)).to be true
-        expect(monopoly_with_fourth_tier.values[0][:names].values[1].has_key?("mortgage_value")).to be true
+        expect(monopoly_with_fourth_tier.values[0][:names].values[1].has_key?(:mortgage_value)).to be true
         expect(monopoly_with_fourth_tier.values[0][:names].values[1].values[0]).to eq("$200")
       end
 
